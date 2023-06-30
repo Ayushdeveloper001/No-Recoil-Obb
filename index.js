4cdd85df2451354614689ae963a09a144eedac34
Ayush Pandey <117559953+Ayushdeveloper001@users.noreply.github.com>
2023-06-30 14:50:04 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+start.sync {let x + devyansh == main.17325.com.pubg.imobile.obb 
+            if true;
+            start.sync
+
+            if false;
+            start.lib
+            let x = bgmi.lib 
+const devyash = "main.17325.com.pubg.imobile.obb"
+            
+let recoil = 1-1
+            if recoil === 0 
+let recoil.Lib + devyansh = devyansh === lib
+
+if x try Connection == lost reconnecting
+    if Connection = true
+            change.value try { recoil + devyansh + "obb is successfully working
